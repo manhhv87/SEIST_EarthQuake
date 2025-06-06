d0ea0ec473a39cb70ca8b27b5c94963198c8e34b
manhhv87 <manhhv87.com>
2025-06-07 00:48:20 +0700

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=MT
+#SBATCH --partition=gpu
+##SBATCH --gres=gpu:0
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --output=%x.%j.out
+#SBATCH --error=%x.%j.err
+#SBATCH --error=%x.%j.err
 
-dt=`date +'%Y-%m-%d_%H-%M-%S'`
+### init virtual environment if needed
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate eq
 
-nohup python -u main.py > logs/log_$dt.log 2>&1 &
+module load singularity
+
+### the command to run
+singularity exec --nv $HOME/pytorch/pytorch-1.13.1-cuda11.6-cudnn8-py3.10 python3 main.py \
+  --seed 0 \
+  --mode "train_test" \
+  --model-name "seist_m_dpk" \
+  --log-base "./logs" \
+  --device "cuda:0" \
+  --data "./data/STEAD" \
+  --dataset-name "stead" \
+  --data-split true \
+  --train-size 0.8 \
+  --val-size 0.1 \
+  --shuffle true \
+  --workers 2 \
+  --in-samples 8192 \
+  --augmentation true \
+  --epochs 200 \
+  --patience 30 \
+  --batch-size 500
